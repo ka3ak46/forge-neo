b75520458484910e61e9e9aa7eb56b79c1646049
ka3ak46 <ka3ak46@gmail.com>
2026-06-09 20:54:09 +0300

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,13 +1,13 @@
 @echo off
 
-:: set PYTHON=
-:: set GIT=
-:: set VENV_DIR=
+ set PYTHON=C:\Python\Python314\python.exe
+ set GIT=
+ set VENV_DIR=
 
-set COMMANDLINE_ARGS=--uv --cuda-malloc --skip-version-check
+set COMMANDLINE_ARGS= --uv --cuda-malloc --skip-version-check
 
-:: --xformers --sage --uv
-:: --pin-shared-memory --cuda-malloc --cuda-stream
-:: --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install
+ --xformers --sage --uv
+ --pin-shared-memory --cuda-malloc --cuda-stream
+ --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install
 
 call webui.bat
